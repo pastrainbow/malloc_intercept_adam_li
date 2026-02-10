0d911068bd1f54eeb1ea270dc47cc1f83d81ffe0
Adam Li <al4624@ic.ac.uk>
2026-02-10 02:18:02 +0000

    Feat: add malloc and dlsym NULL return guarding in malloc_debug.c

--- malloc_debug.c
+++ malloc_debug.c
@@ -7,6 +7,7 @@
 #include <stdint.h>
 #include <pthread.h>
 #include <stdatomic.h>
+#include <assert.h>
 
 // Suppose size_t is n bytes large, then number of decimal digits we need is
 // ⌊log10​(2^(8n)−1)⌋+1
@@ -22,7 +23,8 @@ static pthread_once_t once_control = PTHREAD_ONCE_INIT;
 
 static void* (*real_malloc)(size_t);
 static void init_real_malloc(void) {
-  real_malloc = dlsym(RTLD_NEXT, "malloc"); 
+  real_malloc = dlsym(RTLD_NEXT, "malloc");
+  assert(real_malloc != NULL);
 }
 
 static const char info_prefix[] = "Total bytes allocated so far: ";
@@ -43,32 +45,38 @@ void* malloc(size_t size) {
   // pthread_once does not call malloc(), so safe
   pthread_once(&once_control, init_real_malloc);
 
-  // atomic_fetch_add is atomic at hardware level, so no malloc is used
-  size_t new_total = atomic_fetch_add(&total_bytes_allocated, size) + size;
+  void *real_malloc_result = real_malloc(size);
 
-  // We have the prefix, the number, the line break, and finally the null
-  // terminator
-  char info[info_prefix_length + SIZE_T_MAX_DIGIT_COUNT + 2];
+  if (real_malloc_result != NULL) {
+    // atomic_fetch_add is atomic at hardware level, so no malloc is used
+    size_t new_total = atomic_fetch_add(&total_bytes_allocated, size) + size;
 
-  strncpy(info, info_prefix, info_prefix_length);
+    // We have the prefix, the number, the line break, and finally the null
+    // terminator
+    char info[info_prefix_length + SIZE_T_MAX_DIGIT_COUNT + 2];
 
-  // Copy size's string representation to the current end of info, which should
-  // be just the end of the prefix
-  // Also need the size of the entire info string, so that we can write info to
-  // stderr without truncation or trailing garbage characters
-  int info_str_size = 
-    info_prefix_length + 
-    // We are just formatting a size_t, so snprintf should not allocate memory
-    snprintf(
-      info + info_prefix_length,
-      // Plus 1 for the line break 
-      SIZE_T_MAX_DIGIT_COUNT + 1, 
-      "%zu\n", new_total
-    );
-  
-  // Cannot use printf here, since it uses malloc, meaning that we will
-  // run into infinite recursion. We could make printf use the real malloc
-  // instead of this overriding one, but write works and is simpler to implement 
-  write(STDERR_FILENO, info, (size_t)info_str_size);
-  return real_malloc(size);
+    strncpy(info, info_prefix, info_prefix_length);
+
+    // Copy size's string representation to the current end of info, which 
+    // should be just the end of the prefix
+    // Also need the size of the entire info string, so that we can write info 
+    // to stderr without truncation or trailing garbage characters
+    int info_str_size = 
+      info_prefix_length + 
+      // We are just formatting a size_t, so snprintf should not allocate memory
+      snprintf(
+        info + info_prefix_length,
+        // Plus 1 for the line break 
+        SIZE_T_MAX_DIGIT_COUNT + 1, 
+        "%zu\n", new_total
+      );
+    
+    // Cannot use printf here, since it uses malloc, meaning that we will
+    // run into infinite recursion. We could make printf use the real malloc
+    // instead of this overriding one, but write works and is simpler to 
+    // implement 
+    write(STDERR_FILENO, info, (size_t)info_str_size);
+  }
+
+  return real_malloc_result;
 }
